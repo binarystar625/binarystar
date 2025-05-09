80c07753d118a7201bb7fac1084452b25f3221c1
binarystar <mmrg05469@gmail.com>
2025-05-10 08:57:04 +0900

    @12341234

---
+++ @123123.py
@@ -0,0 +1,14 @@
+class student:
+    def __init__(self, student):
+        self.student
+        self.student = set()
+    def average(self):
+        self.average(student)
+
+n = int(input('학생 수 입력(n): '))
+for n in range(1, n+1, 1):
+    s1 = int(input(f'n번째 학생의 국어성적입력: '))
+    s2 = int(input(f'n번째 학생의 수학성적입력: '))
+    s3 = int(input(f'n번째 학생의 영어성적입력: '))
+    student = {s1, s2, s3}
+    print(f'{n}번째 학생의 평균: {student, average}')
